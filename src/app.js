692e3fa8f142f3ff0636b214204e023a58243fd6
AnastasiiaKo <111301924+AnastasiiaKo@users.noreply.github.com>
2022-10-07 21:24:38 +0100

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -31,19 +31,14 @@ function displayWeatherCondition (response){
   document.querySelector("#min-temp").innerHTML = Math.round(response.data.main.temp_min);
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#humidity").innerHTML = Math.round(response.data.main.humidity);
-  document.querySelector("#cloudy").innerHTML =
-    response.data.weather[0].description;
-
-  let datetoday = document.querySelector("#day-time-today");
-  datetoday.innerHTML = formatDate(response.data.dt * 1000);
-  console.log(response.data);
-
-
-  let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute(
+  document.querySelector("#cloudy").innerHTML = response.data.weather[0].description;
+  document.querySelector("#day-time-today").innerHTML = formatDate(response.data.dt * 1000);
+  document.querySelector("#icon").setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  celsiusTemperature = response.data.main.temp;
 }
 
 
@@ -76,82 +71,40 @@ function getCurrentLocations(event){
   navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
+function displayFarenheitTemperature(event){
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = Math.round((temperatureElement.innerHTML * 9) / 5 + 32);
+  temperatureElement.innerHTML = fahrenheitTemperature;
+}
+
+function displayCelsiusTemperature (event){
+event.preventDefault();
+celsiusLink.classList.add("active");
+fahrenheitLink.classList.remove("active");
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
 let formSearchSity = document.querySelector("#search-form");
 formSearchSity.addEventListener("submit", handleSubmit);
 
 let now = new Date();
-// let today = document.querySelector("#day-time-today");
-// today.innerHTML = formatDate(today);
+
+let celsiusTemperature = null;
 
 let currentLocationButton = document.querySelector("#button-location");
 currentLocationButton.addEventListener("click", getCurrentLocations);
 
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFarenheitTemperature)
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
-
-
-/*function convertToCelsius(event) {
-  event.preventDefault();
-  let temperatureElementC = document.querySelector("#temperature");
-  let temperature = temperatureElementC.innerHTML;
-  temperatureElementC.innerHTML = `${temperature}`;
-   
-  
-}*/
-
-/*function convertToFahrenheit(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = temperatureElement.innerHTML;
-  temperature = Number(temperature);
-  temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
-}*/
-
-/*let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", convertToCelsius);*/
-
-/*let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-let weather = {
-  paris: {
-    temp: 19.7,
-    humidity: 80,
-  },
-  tokyo: {
-    temp: 17.3,
-    humidity: 50,
-  },
-  lisbon: {
-    temp: 30.2,
-    humidity: 20,
-  },
-  "san francisco": {
-    temp: 20.9,
-    humidity: 100,
-  },
-  oslo: {
-    temp: -5,
-    humidity: 20,
-  },
-};*/
-
-/*let sity = prompt("Enter a sity?");
-//sity = sity.toLowerCase();
-if (weather[sity] !== undefined) {
-  let temperature = weather[sity].temp;
-  let humidity = weather[sity].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    `It is currently ${celsiusTemperature} (${fahrenheitTemperature}) in ${sity} with a humidity of ${humidity}%`
-  );
-} else {
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${sity}`
-  );
-}*/
 
 
 
